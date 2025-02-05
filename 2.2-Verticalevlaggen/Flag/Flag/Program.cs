13ec3dff322a6551879c35bb3ad3221c563a1c2f
Aceist <79134787+Aceistt@users.noreply.github.com>
2025-02-05 15:09:07 +0100

    Add files via upload

---
+++ 2.2-Verticalevlaggen/Flag/Flag/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Print de vlag
+        for (int i = 0; i < 12; i++)
+        {
+            PrintFlagLine();
+        }
+    }
+
+    static void PrintFlagLine()
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write("█████████████");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("█████████████");
+        Console.ForegroundColor = ConsoleColor.DarkYellow; // Changed from Orange to DarkYellow
+        Console.WriteLine("█████████████");
+        Console.ResetColor(); // Reset the console color after printing each line
+    }
+}
