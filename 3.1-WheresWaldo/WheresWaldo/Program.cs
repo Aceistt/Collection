46f87a668583f27e511aaa74aef837ee1e9d151d
Aceist <79134787+Aceistt@users.noreply.github.com>
2025-02-11 13:35:21 +0100

    added native 3.1 whereiswaldo

---
+++ 3.1-WheresWaldo/WheresWaldo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace WheresWaldo
+{
+    class Program
+    {
+        // Move variables to class level
+        static string waldo = "Ik ben hier!";
+        static int waldoLocation = 42;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welkom bij 'Where's Waldo'!");
+
+            ZoekWaldo();
+            ToonWaldo();
+        }
+
+        static void ZoekWaldo()
+        {
+            // Access class-level variable 'waldoLocation'
+            Console.WriteLine($"Waldo bevindt zich op locatie {waldoLocation}!");
+        }
+
+        static void ToonWaldo()
+        {
+            // Access class-level variable 'waldo'
+            Console.WriteLine(waldo);
+        }
+    }
+}
